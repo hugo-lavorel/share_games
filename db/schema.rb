0b431530fed95ac90391d6f1c45b8ce142518f7f
Hugo Lavorel <hugo.lavorel@gmail.com>
2020-10-11 20:00:05 +0200

    Adding game description in the model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_091415) do
+ActiveRecord::Schema.define(version: 2020_10_11_175704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_10_10_091415) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.text "description"
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
