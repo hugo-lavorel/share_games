0c6a6ff967f8c42dc518a055db4eb4074f837d39
Hugo Lavorel <hugo.lavorel@gmail.com>
2020-11-10 15:13:13 +0100

    New master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_111512) do
+ActiveRecord::Schema.define(version: 2020_11_08_171822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,9 @@ ActiveRecord::Schema.define(version: 2020_10_27_111512) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.text "description"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
