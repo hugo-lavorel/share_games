3562474eb59900efe1b19f726eee507ce0c5dd54
Hugo Lavorel <hugo.lavorel@gmail.com>
2020-11-13 14:28:07 +0100

    Installation de Flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,10 +29,12 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { initMapbox } from '../plugins/init_mapbox';
+import { initFlatpickr} from '../plugins/init_flatpickr'
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initAutocomplete();
   initMapbox();
+  initFlatpickr();
 });
---
+++ app/javascript/plugins/init_flatpickr.js
@@ -0,0 +1,7 @@
+import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+
+flatpickr("#range_start", {
+  altInput: true,
+  plugins: [new rangePlugin({ input: "#range_end"})]
+});
